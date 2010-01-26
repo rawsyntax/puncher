d0044efb7c1cbdaf841ab02a50d67ce6534a32d6
Eric Himmelreich <eahimmel@gmail.com>
2010-01-25 22:02:20 -0500

    enough for a 0.0.1 release

---
+++ lib/puncher.rb
@@ -0,0 +1,39 @@
+begin
+  require 'fsevent'
+rescue LoadError
+  require 'rubygems'
+  require 'fsevent'
+end
+
+class Puncher < FSEvent
+
+  def initialize(command, files)
+    @command, @files, @latency = command, files, 0.2
+    @last_time = Time.now
+
+    start
+  end
+
+  def start
+    @dirs = []
+
+    @files.each do |f|
+      @dirs << File.dirname(f)
+    end
+
+    watch_directories(@dirs.uniq)
+    super
+  end
+
+  def on_change(directories)
+    time = @last_time
+
+    @files.each do |f|
+      if File.mtime(f) > @last_time
+        @last_time = File.mtime(f)
+      end
+    end
+
+    system @command unless time == @last_time
+  end
+end
--- spec/puncher_spec.rb
+++ spec/puncher_spec.rb
@@ -1,7 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Puncher" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Puncher do
+  describe 'initialize' do
+    it 'should create a puncher' do
+      @puncher = Puncher.new("echo 'boosh'", '.*')
+
+      @puncher.should_not be_nil
+    end
   end
 end
