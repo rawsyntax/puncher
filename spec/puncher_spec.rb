a235e44a73248e4b185226297b32acfb1bbb122b
Eric Himmelreich <eahimmel@gmail.com>
2010-01-29 10:20:13 -0500

    remove stupid specs.. have to think more about how to test this

--- spec/puncher_spec.rb
+++ spec/puncher_spec.rb
@@ -1,11 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Puncher do
-  describe 'initialize' do
-    it 'should create a puncher' do
-      @puncher = Puncher.new("echo 'boosh'", '.*')
-
-      @puncher.should_not be_nil
-    end
+  it 'should have specs' do
+    pending
   end
 end
